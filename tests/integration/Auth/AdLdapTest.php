f9c8cc6e19c3252db5227578f7e05bd290d4afcc
Michel Vielmetter <git@michelvielmetter.de>
2016-04-06 11:08:02 +0200

    create auth tests

---
+++ tests/integration/Auth/AdLdapTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AdLdapTest extends TestCase
+{
+    use DatabaseTransactions, DatabaseMigrations;
+
+    public $ldap_config = [];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->ldap_config = [
+            'account_suffix'     => env('LDAP_ACCOUNT_SUFFIX'),
+            'domain_controllers' => [env('LDAP_DOMAIN_CONTROLLER')],
+            'base_dn'            => env('LDAP_BASE_DN'),
+            'admin_username'     => env('LDAP_ADMIN_USER'),
+            'port'               => env('LDAP_PORT', 389),
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function can_connect_to_ldap_server()
+    {
+        $adldap = new \Adldap\Adldap($this->ldap_config);
+
+        $this->assertTrue($adldap->getConnection()->isBound());
+    }
+
+    /**
+     * @test
+     *
+     * Check if we can connect
+     *
+     * @return void
+     */
+    public function can_login_with_user()
+    {
+        $auth = Auth::attempt(
+            [
+                'username' => env('LDAP_TEST_USER'),
+                'password' => env('LDAP_TEST_USER_PASS'),
+            ]
+        );
+        $this->assertTrue($auth);
+    }
+}
